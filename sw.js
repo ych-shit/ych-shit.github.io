3db3bc23a5e9d7560d6c0f1bf95c2665a2e3691f
ych-shit <1508284193@qq.com>
2025-02-02 22:21:09 +0800

    Site updated: 2025-02-02 22:21:09

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1738505885248";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1738506067045";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
